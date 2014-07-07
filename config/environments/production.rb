3b36023bf88e96bfeaad08bbb526a217bf7b3a54
Josh Leeman <joshleeman@gmail.com>
2014-07-07 00:56:09 -0400

    production confix default_host setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,8 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { host: 'jleeman-myflix.heroku.com' }
+
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
