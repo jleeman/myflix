12042a7d71b4f740ca544e75b13fdc5c3e0583a7
Josh Leeman <joshleeman@gmail.com>
2014-07-15 22:18:48 -0400

    carrierwave uploader updates

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,6 +5,9 @@ class Video < ActiveRecord::Base
   # rails 4 now requires scope blocks:
   has_many :reviews, -> { order 'created_at DESC' }
 
+  mount_uploader :large_cover, LargeCoverUploader
+  mount_uploader :small_cover, SmallCoverUploader
+
   validates_presence_of :title, :description
 
   def self.search_by_title(search_term)
