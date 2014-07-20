fc37f25dffff4b92ceffbe5e6faf3747c4d34301
Josh Leeman <joshleeman@gmail.com>
2014-07-20 15:30:47 -0400

    carrierwave edit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,14 @@
 CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
+    Figaro.require("AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "AWS_BUCKET")
     config.storage = :fog
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
       :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],                        # required
-      :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],                        # required
+      :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"]                        # required
     }
-    config.fog_directory  = ENV["AWS_BUCKET"]                     #
+    config.fog_directory  = ENV["AWS_BUCKET"]
+    config.fog_public = false
   else
     config.storage = :file
     config.enable_processing = Rails.env.development?
