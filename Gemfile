1c977136d2e26df0f9d89cab0cdbb5e6ad94196c
Josh Leeman <joshleeman@gmail.com>
2014-07-21 20:04:47 -0400

    not working, secret/publishable key problem

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ group :test do
   gem 'capybara'
   gem 'capybara-email', github: 'dockyard/capybara-email'
   gem 'launchy'
+  gem 'webmock'
+  gem 'vcr'
 end
 
 group :production do
---
+++ spec/models/stripe_wrapper_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+describe StripeWrapper do
+  describe StripeWrapper::Charge do
+    before do
+      VCR.use_cassette('set api key') do
+        StripeWrapper::Charge.set_api_key
+      end
+    end
+
+    let(:token) do
+      VCR.use_cassette('let token') do
+        Stripe::Token.create(
+          :card => {
+            :number => card_number,
+            :exp_month => 3,
+            :exp_year => 2018,
+            :cvc => 123
+          }
+        ).id
+      end
+    end
+
+    describe ".create" do
+    end
+
+    context "with valid card" do
+      let(:card_number) { '4242424242424242' }
+
+      it "charges the card successfully" do
+        VCR.use_cassette('create charge with valid card') do
+          response = StripeWrapper::Charge.create(amount: 300, card: token)
+        end
+        expect(response).to be_successful
+      end
+    end
+
+
+    context "with invalid card" do
+      let(:card_number) { '4000000000000002' }
+
+      let(:response) do
+        VCR.use_cassette('create charge with invalid card') do
+          StripeWrapper::Charge.create(amount: 300, card: token)
+        end
+      end
+
+      it "does not charge the card" do
+        expect(response).not_to be_successful
+      end
+
+      it "contains an error message" do
+        response.error_message.should == "Your card was declined."
+      end
+    end
+  end
+end
